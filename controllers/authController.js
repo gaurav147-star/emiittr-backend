7af799571b36613d1a03da51dfbbea97fe729b28
gaurav147-star <gauravgupta9158@gmail.com>
2023-12-06 12:23:32 +0530

    changes in auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,19 +9,19 @@ const generateToken = (user) => {
 const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-
     const userExists = await User.findOne({ email });
 
     if (userExists) {
-      res.status(400);
-      throw new Error("User already exists");
+      return res.status(400).json({ error: "User already exists" }); // Send error response and return to avoid further execution
     }
 
     const salt = bcrypt.genSaltSync(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const user = new User({ name, email, password: hashedPassword });
-    await user.save();
+    const user = await User.create({ name, email, password: hashedPassword }); // Use 'User.create' to create a new user
+    if (!user) {
+      return res.status(500).json({ error: "User creation failed" }); // Handle user creation failure
+    }
     const token = generateToken(user._id);
     res.json({ token });
   } catch (err) {
